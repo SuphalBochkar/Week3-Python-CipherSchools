c1ac0801f8edac48c32051d9bc9bcd55b30a7ee4
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-11 11:49:40 +0530

    V-8

---
+++ Class134-Compren if-CipherSchools.py
@@ -0,0 +1,11 @@
+
+num = list(range(1,11))
+
+even_nums = [i for i in num if i%2==0]
+print(even_nums)
+even1_nums = [i for i in range(1,11) if i%2==0]
+print(even1_nums)
+
+odd_nums = [i for i in num if i%2!=0]
+print(odd_nums)
+
