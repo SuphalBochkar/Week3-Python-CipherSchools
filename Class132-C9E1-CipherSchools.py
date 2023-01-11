392eac8736ba8a02df607dc954044946b1a9295b
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-11 11:48:38 +0530

    V-6

---
+++ Class132-C9E1-CipherSchools.py
@@ -0,0 +1,11 @@
+# define a function that take list of strings
+# list containing reverse of every string
+# USE LIST COMPREHENSION because we already did this exercise
+# NOTE -
+# using normal method
+# example
+# ['abc', 'tuv
+# reverse_string(l) -
+# xyz ' ]
+# , 'üut', 'zyx']
+
