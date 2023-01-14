95d7d88712c66a7a8833e94e64046596aff5916a
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-15 00:33:33 +0530

    .

---
+++ Class153-Lambda-CipherSchools.py
@@ -0,0 +1,14 @@
+# lambda expressions (anonymous function)
+def add(a,b):
+    return a+b
+add2 = lambda a,b : a+b
+print(add2(5,6))
+
+mul = lambda a,b : a*b
+print(mul(2,9))
+
+
+print(add)
+print(add2)
+print(mul)
+
