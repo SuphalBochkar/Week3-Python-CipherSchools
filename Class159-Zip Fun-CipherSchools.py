55b13c0738934cc7df137e8998e356ac58824adc
SuphalBochkar <suphalbochkar@gmail.com>
2023-02-03 23:44:57 +0530

    Update Class159-Zip Fun-CipherSchools.py

--- Class159-Zip Fun-CipherSchools.py
+++ Class159-Zip Fun-CipherSchools.py
@@ -4,3 +4,8 @@ user = ['user1','user2','user3']
 names = ['suphal','sai','bochkar']
 l_name = ['Bochkar','bochkar','Bochkar']
 print(list(zip(user,names)))     #! List
+
+print(dict(zip(user,names)))     #! Dictionary
+
+print(list(zip(user,names,l_name)))     #! List of 3
+print(dict(zip(user,names,l_name)))     #! #f2f Error
