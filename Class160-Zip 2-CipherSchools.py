93d492d641049fa736ebc882587e151ae8955414
SuphalBochkar <suphalbochkar@gmail.com>
2023-02-04 23:45:09 +0530

    Update Class160-Zip 2-CipherSchools.py

--- Class160-Zip 2-CipherSchools.py
+++ Class160-Zip 2-CipherSchools.py
@@ -7,3 +7,13 @@ l = [(1,2),(3,4),(5,6),(7,8)]   #! Goal
 # * operator with zip
 
 # print(list(zip(*l)))
+# l1,l2 = zip(*l)
+# print(list(l1))
+# print(l2)
+
+maxq = []
+for pair in zip(l1,l2):
+    maxq += [max(pair)]
+print(maxq)
+
+
