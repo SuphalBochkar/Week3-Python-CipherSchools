eca1b3ff928f7702b88b40e89c87e6db729011b5
SuphalBochkar <suphalbochkar@gmail.com>
2023-02-05 23:45:22 +0530

    Update Class161-Advance Fun Pra-CipherSchools.py

--- Class161-Advance Fun Pra-CipherSchools.py
+++ Class161-Advance Fun Pra-CipherSchools.py
@@ -3,3 +3,14 @@
 # return average
 # ( 1+4+7 ) / 3
 # try to make this
+# anonymous
+# no of lists containing number
+# function in one line using lambda
+
+#! M-1
+# def ave1(l1,l2):
+#     average = []
+#     for pair in zip(l1,l2):
+#         average.append(sum(pair)/len(pair))
+#     return average
+#! M-2
