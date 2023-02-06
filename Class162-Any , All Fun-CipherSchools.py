782a4da592247052e441515122bdf1659ba27662
SuphalBochkar <suphalbochkar@gmail.com>
2023-02-06 23:45:55 +0530

    Update Class162-Any , All Fun-CipherSchools.py

--- Class162-Any , All Fun-CipherSchools.py
+++ Class162-Any , All Fun-CipherSchools.py
@@ -9,3 +9,8 @@ num2 = [1,2,3,4,5,6]
 # print(all([1,1,2,1,1]))  #! ------> False
 
 # print([i%2==0 for i in num1])
+# print(all([i%2==0 for i in num1]))
+
+#& Any
+print(any([1,1,1,1,1]))  #! ------> True
+print(any([7,5,2,1,3]))  #! ------> True
