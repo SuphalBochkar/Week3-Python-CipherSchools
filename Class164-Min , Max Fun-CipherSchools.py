d4acf5dc67c07412f3be9338e0c6e09e77257504
SuphalBochkar <suphalbochkar@gmail.com>
2023-02-08 23:46:20 +0530

    Update Class164-Min , Max Fun-CipherSchools.py

--- Class164-Min , Max Fun-CipherSchools.py
+++ Class164-Min , Max Fun-CipherSchools.py
@@ -5,6 +5,13 @@
 # print(max(num))
 # print(min(num))
 
+names  = ['suphal','bochkar','sai']
+#! M-1
+# print(max([len(i) for i in names]))
+#! M-2
+# print(max(names,key = lambda item : len(item)))
+# print(sorted(names,key = lambda item : len(item),reverse=True))           #f2f #^ Most Important 
+
 
 
 
