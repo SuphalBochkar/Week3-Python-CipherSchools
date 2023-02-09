f5ed823adb76f84f214992ef529950b4e3b7a3be
SuphalBochkar <suphalbochkar@gmail.com>
2023-02-09 23:46:42 +0530

    Update Class165-Sorted() Fun-CipherSchools.py

--- Class165-Sorted() Fun-CipherSchools.py
+++ Class165-Sorted() Fun-CipherSchools.py
@@ -5,3 +5,10 @@
 # fru1.sort()
 # print(fru1)
 
+#& Sorted ----> Tupul
+# fru2 = ('grapes','mango','apple')
+# print(sorted(fru2))
+
+#& Sorted ----> Set
+# fru3 = {'grapes','mango','apple'}
+# print(sorted(fru3))
