948ce97c31f0c4633f4eb4937c24969b003801a6
SuphalBochkar <suphalbochkar@gmail.com>
2023-02-09 23:46:45 +0530

    Update Class165-Sorted() Fun-CipherSchools.py

--- Class165-Sorted() Fun-CipherSchools.py
+++ Class165-Sorted() Fun-CipherSchools.py
@@ -12,3 +12,11 @@
 #& Sorted ----> Set
 # fru3 = {'grapes','mango','apple'}
 # print(sorted(fru3))
+
+guitars = [
+    {'model': 'yamaha', 'price' : 840},
+    {'model': 'bez', 'price' : 112},
+    {'model': 'yalchu', 'price' : 232},
+    {'model': 'marge', 'price' : 781},
+]
+print(sorted(guitars,key = lambda d:d['price'],reverse=True))
