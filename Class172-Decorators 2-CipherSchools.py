23fd8dbbba10d185381b1c5d82d0d0afee5d9f1c
SuphalBochkar <suphalbochkar@gmail.com>
2023-02-18 16:39:16 +0530

    Update Class172-Decorators 2-CipherSchools.py

--- Class172-Decorators 2-CipherSchools.py
+++ Class172-Decorators 2-CipherSchools.py
@@ -3,3 +3,8 @@ def deco_func(any_func):
         print('This is awesome Function')
         return any_func(*args, **kwargs)
     return wrapper
+
+@deco_func
+def func(a):
+    print(f'this is a function eith an argument {a}')
+func(2)
