72784ae9e6ef53302fbdf9cc0ca53d2926a46440
SuphalBochkar <suphalbochkar@gmail.com>
2023-02-26 16:44:59 +0530

    Update Class186-Oops Class-CipherSchools.py

--- Class186-Oops Class-CipherSchools.py
+++ Class186-Oops Class-CipherSchools.py
@@ -4,3 +4,10 @@
 # WHAT IS INIT METHOD
 # WHAT ARE ATTRIBUTES, INSTANCE VARIABLES
 # HOW TO CREATE OUR OBJECT
+
+class Person:
+    def __init__(self,fn,ln,age):
+        # instace variables
+        print("init method is called")
+        self.fn = fn
+        self.ln = ln
