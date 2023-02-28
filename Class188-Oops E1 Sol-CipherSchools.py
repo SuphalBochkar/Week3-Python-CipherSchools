7c2868c8e14dcecd5ded358ead1acbcd20aaee46
SuphalBochkar <suphalbochkar@gmail.com>
2023-02-28 16:45:30 +0530

    Create Class188-Oops E1 Sol-CipherSchools.py

---
+++ Class188-Oops E1 Sol-CipherSchools.py
@@ -0,0 +1,7 @@
+
+class Laptop:
+    def __init__(self,name,model,price):
+        self.name = name
+        self.model = model
+        self.price = price
+        self.details = "this is " + name + ' laptop is costs ' + price
