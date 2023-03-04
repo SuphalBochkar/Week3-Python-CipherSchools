21f5816d76f13739cf0d83120c1ae1001037bbfc
SuphalBochkar <suphalbochkar@gmail.com>
2023-03-04 16:48:59 +0530

    Update Class191-Oops E2 Sol-CipherSchools.py

--- Class191-Oops E2 Sol-CipherSchools.py
+++ Class191-Oops E2 Sol-CipherSchools.py
@@ -6,3 +6,13 @@ class Laptop:
         self.name = name
         self.price = price
     
+    def discount(self):
+        # a = int(input())
+        return f'Discount Price is {self.price - ((int(input())/100)*self.price)}'
+
+lap1 = Laptop('dell','inspron133',63000)
+# lap2 = Laptop('hp','pavallien',47000)
+
+print(lap1.discount())
+# print(lap2.discount())
+
