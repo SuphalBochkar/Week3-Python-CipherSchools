9973b2eb9610ce5346138b83f247a56106e60fcc
Suphal Bochkar <suphalbochkar@gmail.com>
2023-07-02 18:10:02 +0530

    Class Circle

---
+++ Class192-Class VAriable-CipherSchools.py
@@ -0,0 +1,16 @@
+
+#~ Circle
+# class Circle:
+#     pi = 3.14
+#     def __init__(self,radius):
+#         self.radius = radius    
+#     def circum(self):
+#         return 2*Circle.pi*self.radius
+#     def area(self):
+#         return Circle.pi*(self.radius**2)
+# obj1 = Circle(5)
+# obj2 = Circle(7)
+# print(obj1.circum())
+# print(obj2.circum())
+# print(obj1.area())
+# print(obj2.area())
