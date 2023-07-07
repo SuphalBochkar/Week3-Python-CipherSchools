f94cd5c57ff5d4c99b390b5df2f0bab6270c2a58
Suphal Bochkar <suphalbochkar@gmail.com>
2023-07-07 18:11:46 +0530

    Count

---
+++ Class196-Class Method-CipherSchools.py
@@ -0,0 +1,12 @@
+# class methods
+# difference between class methods and instance methods
+#& Class-Varible Syntax
+#^ Class_Name.varible = constant
+#& Class-Method Syntax
+#^ Class_Name.Method_Name()
+obj1 = Person('suphal','bochkar',18)   #^ Instance
+obj2 = Person('sai','bochkar',17)
+
+
+
+print(Person.count_instances())
