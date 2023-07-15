349870fea55d5779dd435a3d035cf577f2052f5e
Suphal Bochkar <suphalbochkar@gmail.com>
2023-07-15 13:18:00 +0530

    Phone

--- Class201-Inheritance-CipherSchools.py
+++ Class201-Inheritance-CipherSchools.py
@@ -12,3 +12,18 @@ class Phone:                #^ Base / Parent Class
     def make_a_call(self,number):         
         print(f'Calling {number}...... form {self.name}')
 
+class Smartphone(Phone):         #^ Derived / Child Class
+
+    def __init__(self, name, model, price,ram,memory,cam):
+
+        # Phone.__init__(self,name,model,price)
+        super().__init__(name, model, price)
+        self.ram = ram
+        self.memory = memory
+        self.cam = cam
+
+phone1 = Phone('nokia','1100',1000)
+smart1 = Smartphone('onePlus','note 10',3000,'6 GB','128 GB','20 MP')
+print(phone1.full_name())
+print(smart1.full_name() + f' and price is {smart1.cam}')
+
